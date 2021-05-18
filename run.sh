fe8d86372b8661d09770b86fa1a7ba56aeed8a51
Brad Geesaman <bradgeesaman+github@gmail.com>
2021-05-17 20:44:09 -0400

    start date

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ IMAGE_PATH="gcr.io/opencspm/cspm-lite:latest"
 CURRENT_DIR="$(PWD)"
 COMMAND="${@:-rake -T}"
 HOMEDIR="${HOME}"
+START_DATE="2021-03-15"
 #PROJECT_ID="$(gcloud config get-value project)"
 PROJECT_ID="db-tenant-32d72099"
 BUCKET_NAME="db-tenant-32d72099-us-opencspm"
@@ -11,6 +12,7 @@ docker run --rm -it --net="docker_cspm" \
   -v "${CURRENT_DIR}/data:/app/data" \
   -v "${CURRENT_DIR}/../opencspm-controls:/opencspm-controls" \
   -v "${HOMEDIR}/.config/gcloud:/root/.config/gcloud" \
+  -e RAKE_START_DATE="${START_DATE}" \
   -e RAKE_PROJECT_ID="${PROJECT_ID}" \
   -e RAKE_BUCKET_NAME="${BUCKET_NAME}" \
   -e GOOGLE_AUTH_SUPPRESS_CREDENTIALS_WARNINGS=true \
